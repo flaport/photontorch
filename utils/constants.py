947b60afaf8c2a6bec752c048962568d36a7f562
Floris Laporte <floris.laporte@gmail.com>
2018-01-23 12:28:26 +0100

    added some useful constants

--- utils/constants.py
+++ utils/constants.py
@@ -1,6 +1,25 @@
 ''' Useful constants for photonic simulations '''
 
-from numpy import pi
+#############
+## Imports ##
+#############
+
+import numpy as np
+
+
+###############
+## Constants ##
+###############
+
+# pi
+pi = np.pi
 
 # speed of light
 c = 299792458.0
+
+# standard room temperature:
+T0 = 300
+
+# conversion between FWHM and sigma:
+sigma2fwhm = 2*np.sqrt(2*np.log(2))
+fwhm2sigma = 1.0/sigma2fwhm
