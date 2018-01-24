2465ef0b1142641350ad6fd078a4ab5984ae86fb
Floris Laporte <floris.laporte@gmail.com>
2018-01-24 12:02:48 +0100

    updated grating coupler

--- __init__.py
+++ __init__.py
@@ -10,6 +10,7 @@ from .components import Network
 from .components import Detector
 from .components import Waveguide
 from .components import SlantedMirror
+from .components import GratingCoupler
 from .components import DirectionalCoupler
 
 
