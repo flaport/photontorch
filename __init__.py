2ef5cad48defc50b87f60a167819e3d46d98d31f
Floris Laporte <floris.laporte@gmail.com>
2018-01-25 10:17:43 +0100

    created seperate folder for networks

--- __init__.py
+++ __init__.py
@@ -1,17 +1,30 @@
 ''' A package using the machine-learning module PyTorch to easily optimize Photonic Circuits '''
 
+# Useful while in development...
+import sys
+sys.dont_write_bytecode = True
+
 
 ## Components
 
 from .components import Term
 from .components import Source
 from .components import Mirror
-from .components import Network
 from .components import Detector
 from .components import Waveguide
 from .components import SlantedMirror
 from .components import GratingCoupler
 from .components import DirectionalCoupler
+
+
+## Network
+
+from .network import Network
+
+
+## Environment
+
+from .environment import Environment
 
 
 ## Useful utils
