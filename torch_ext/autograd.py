68685046fdb28c9f729e0676586adab14c03899f
Floris Laporte <floris.laporte@gmail.com>
2018-04-09 17:22:22 +0100

    removed the pseudo inverse as it was not used anymore

--- torch_ext/autograd.py
+++ torch_ext/autograd.py
@@ -18,7 +18,6 @@ The classes defined below are called inside these functions and should not be us
 
 ## Torch
 import torch
-from torch.autograd import Variable
 from torch.autograd import Function
 
 ## Other
@@ -26,108 +25,6 @@ import numpy as np
 
 ## Relative
 from .tensor import zeros
-
-
-###################
-## PseudoInverse ##
-###################
-
-## PseudoInverse Function
-def pinv(input, rcond=1e-8):
-    ''' Moore-Penrose Pseudo Inverse of a 2D FloatTensor.
-
-    Calculated using the singular-value decomposition with cutoff `rcond`.
-
-    Args:
-        input (2D torch.Tensor): Matrix to be pseudo-inverted.
-        rcond (float): Cutoff for small singular values.
-
-    Returns:
-    2D torch.Tensor: The pseudo-inverse of `input`.
-
-    '''
-    return PseudoInverse().apply(input, rcond=rcond)
-
-
-class PseudoInverse(Function):
-    ''' Moore-Penrose Pseudo Inverse of a 2D FloatTensor.
-
-    Calculated using the singular-value decomposition with cutoff `rcond`.
-
-    Note:
-        Do not use this torch Autograd Function Class directly. Use `pinv` in stead.
-
-    '''
-    @staticmethod
-    def forward(ctx, input, rcond=1e-8):
-        ''' PseudoInverse forward
-
-        The forward method calculates the pseudo-inverse and saves the necessary variables
-        for a backward pass in the context variable.
-
-        Args:
-            ctx (torch context): An object storing the information for backward pass.
-            input (2D torch.Tensor): Matrix to be pseudo-inverted.
-            rcond (float): Cutoff for small singular values.
-
-        Returns:
-            2D torch.Tensor: The pseudo-inverse of `input`.
-
-        Note:
-            This function is used by PyTorch internally and should not be used by the user.
-            Use the `.apply` function in stead.
-
-        '''
-        cutoff = rcond*input.max()
-        # numpy way:
-        # np_inverse = np.linalg.pinv(np.array(input.cpu().numpy(), dtype=np.float32), rcond=rcond)
-        # inverse = torch.from_numpy(np_inverse)
-
-        # torch way
-        U, S, V = torch.svd(input)
-        selection = (torch.abs(S) <= cutoff)
-        if selection.all():
-            invS = torch.diag(torch.zeros_like(S))
-        else:
-            invS = torch.diag((1./S)*(~selection).type(input.type()))
-        inverse = V.mm(invS.mm(U.t()))
-
-        # save the inverse for the backward pass
-        ctx.inverse = inverse
-
-        # return the inverse
-        return inverse
-
-    @staticmethod
-    def backward(ctx, grad_output):
-        ''' PseudoInverse backward
-
-        The backward pass uses the formula for the derivative of a matrix inverse:
-        ```math
-            M^{-1} = -M^{-1}\cdot M\cdot M^{-1}.
-        ```
-        We assume this formula also works for the pseudo-inverse.
-
-        Args:
-            ctx (torch context): An object for accessing the stored information of
-                the forward pass.
-            grad_output (2d torch.Tensor): Matrix describing the gradient of the next layer.
-
-        Returns:
-            2d torch.Tensor: The gradient with respect to this layer.
-
-        Note:
-            This function is used by PyTorch internally and should not be used by the user.
-
-        '''
-
-        # some debugging with torch.autograd.gradcheck shows that we need
-        # to transpose the inverse, for the desired backward pass to work.
-        # So far, I have no idea why this is the case.
-        inverse = Variable(ctx.inverse, requires_grad=True).t() # we need a variable
-
-        # return the gradient
-        return -inverse.mm(grad_output.mm(inverse))
 
 
 ####################
