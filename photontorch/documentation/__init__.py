7c58135f551f6eb9721c8c7a4177c6eb07d7460a
Floris Laporte <floris.laporte@gmail.com>
2018-11-30 12:57:59 +0100

    added a script that generates the photontorch documentation from docstrings

---
+++ photontorch/documentation/__init__.py
@@ -0,0 +1,11 @@
+"""
+# Documentation
+
+Custom scripts to generate the photontorch documentation
+
+You can generate and view the documentation by typing [only with python 3]
+
+```
+python -m photontorch.documentation
+```
+"""
---
+++ photontorch/documentation/__main__.py
@@ -0,0 +1,9 @@
+""" run the documentation script 
+
+```
+python -m photontorch.documentation
+```
+"""
+from .documentation import main
+
+main()
