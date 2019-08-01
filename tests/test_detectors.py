b031c16fd8b6ce1773cd595cf0bf26321039a2a1
flaport <floris.laporte@gmail.com>
2019-08-01 12:28:20 +0200

    updated photodetector tests to reflect changes in api

--- tests/test_detectors.py
+++ tests/test_detectors.py
@@ -30,8 +30,8 @@ def test_photodetector_forward(det):
     with torch.no_grad():
         num_bits = 10
         bits = torch.rand(num_bits) > 0.5
-        det = pt.Photodetector(bitrate=50e9, dt=1.0 / (10 * 50e9))
-        timesteps_per_bit = int(1.0 / (det.bitrate * det.dt) + 0.5)
+        det = pt.Photodetector(bitrate=50e9, frequency=5 * 50e9)
+        timesteps_per_bit = int(det.frequency / det.bitrate + 0.5)
         bitstream = torch.stack([bits] * timesteps_per_bit, -1).flatten().float()
         detected_bitstream = det(bitstream)
 
